eabb969fc04a943b539bc36cb9534d8e5c9574c5
Esau Jesse <jesseesau13@gmail.com>
2024-12-01 12:28:32 +0100

    add Description field to product model

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     image = models.URLField(max_length=500, blank=True, null=True)
--- products/serializers.py
+++ products/serializers.py
@@ -5,5 +5,5 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price',
+        fields = ['id', 'name', 'description', 'price',
                   'available', 'image', 'created_at']
