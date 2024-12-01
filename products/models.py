640d5e0f7d1fa4998c1623d70f1b39ff3c795768
Esau Jesse <jesseesau13@gmail.com>
2024-12-01 12:35:16 +0100

    Update ImageURL to ImageField in product model.

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
-    image = models.URLField(max_length=500, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
